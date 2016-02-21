a0215c0968cb38be684e6d41e9b5045c82ade962
Peter Lyons <pete@peterlyons.com>
2016-02-21 10:03:11 -0700

    update to latest deps
    MIT license
    standard formatting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#writers-digest
+# writers-digest
 
 Store files on disk based on their SHA1 digest the way git does.
 
@@ -8,38 +8,38 @@ For example, given a digest of `68ce1ed198f097294448ab7a47e4c4922ef46048`, the f
 
 This module exports a single function with the following parameters:
 
-* filePath
-  * path to the existing file to store
-  * can be relative or absolute
-  * this file will be moved (renamed, not copied) into the destination location
-* baseDir (optional)
-  * base directory for the tree of files stored by digest
-  * if omitted, files will be stored below the current directory
-* callback
-  * callback function arguments:
-    * error (if any operations fail)
-    * result (on success)
-       * this will be an object with attributes:
-       * path: full path to the stored file
-       * digest: the 40-char hexidecimal SHA1 digest of the file
+- filePath
+  - path to the existing file to store
+  - can be relative or absolute
+  - this file will be moved (renamed, not copied) into the destination location
+- baseDir (optional)
+  - base directory for the tree of files stored by digest
+  - if omitted, files will be stored below the current directory
+- callback
+  - callback function arguments:
+    - error (if any operations fail)
+    - result (on success)
+       - this will be an object with attributes:
+       - path: full path to the stored file
+       - digest: the 40-char hexidecimal SHA1 digest of the file
 
-#Example usage
+# Example usage
 
 ```javascript
-var store = require("writers-digest");
+var store = require('writers-digest')
 
-store("someFile", function (error, result) {
-  //"someFile" has been moved to path result.path
-  //result.digest is the hexidecimal SHA1 digest of the contents of "someFile"
-  console.dir(result);
-});
+store('someFile', function (error, result) {
+  // 'someFile' has been moved to path result.path
+  // result.digest is the hexidecimal SHA1 digest of the contents of 'someFile'
+  console.dir(result)
+})
 ```
 
-#License: MIT License
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
 
-The MIT License (MIT)
+# License: MIT License
 
-Copyright (c) 2012 Peter Lyons LLC
+Copyright (c) 2016 Peter Lyons LLC
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- tests.js
+++ tests.js
@@ -1,64 +1,64 @@
-var assert = require("chai").assert;
-var async = require("async");
-var crypto = require("crypto");
-var fs = require("fs");
-var path = require("path");
-var store = require("./index");
+var assert = require('chai').assert
+var async = require('async')
+var crypto = require('crypto')
+var fs = require('fs')
+var path = require('path')
+var store = require('./')
 
-var testData = "123456789\n";
-digest = crypto.createHash("sha1");
-digest.update(testData);
-var hex = digest.digest("hex");
-var first2 = hex.slice(0, 2);
-var last38 = hex.slice(2, 40);
+var testData = '123456789\n'
+digest = crypto.createHash('sha1')
+digest.update(testData)
+var hex = digest.digest('hex')
+var first2 = hex.slice(0, 2)
+var last38 = hex.slice(2, 40)
 
-describe("writers-digest success cases", function () {
-  var testStorePath = "test_store";
-  var testFilePath = path.join(testStorePath, "test_file_1");
+describe('writers-digest success cases', function () {
+  var testStorePath = 'test_store'
+  var testFilePath = path.join(testStorePath, 'test_file_1')
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     async.series([
-        async.apply(fs.mkdir, testStorePath),
-        async.apply(fs.writeFile, testFilePath, testData)
-      ], done);
-  });
+      async.apply(fs.mkdir, testStorePath),
+      async.apply(fs.writeFile, testFilePath, testData)
+    ], done)
+  })
 
   var cleanUp = function (done) {
     async.series([
-        async.apply(fs.unlink, path.join(testStorePath, first2, last38)),
-        async.apply(fs.rmdir, path.join(testStorePath, first2)),
-        async.apply(fs.rmdir, testStorePath)
-      ], done);
-  };
+      async.apply(fs.unlink, path.join(testStorePath, first2, last38)),
+      async.apply(fs.rmdir, path.join(testStorePath, first2)),
+      async.apply(fs.rmdir, testStorePath)
+    ], done)
+  }
 
-  it("should work in the base case", function(done) {
+  it('should work in the base case', function (done) {
     store(testFilePath, testStorePath, function (error, result) {
-      assert.isNull(error);
-      assert.equal(hex, result.digest);
-      cleanUp(done);
-    });
-  });
+      assert.isNull(error)
+      assert.equal(hex, result.digest)
+      cleanUp(done)
+    })
+  })
 
-  it("should work if first2 directory exists", function(done) {
+  it('should work if first2 directory exists', function (done) {
     fs.mkdir(path.join(testStorePath, first2), function (error) {
-      assert.isNull(error);
+      assert.isNull(error)
       store(testFilePath, testStorePath, function (error, result) {
-        assert.isNull(error);
-        assert.equal(hex, result.digest);
-        cleanUp(done);
-      });
-    });
-  });
+        assert.isNull(error)
+        assert.equal(hex, result.digest)
+        cleanUp(done)
+      })
+    })
+  })
 
-  it("should work with no baseDir argument", function(done) {
+  it('should work with no baseDir argument', function (done) {
     store(testFilePath, function (error, result) {
-      assert.isNull(error);
-      assert.equal(hex, result.digest);
+      assert.isNull(error)
+      assert.equal(hex, result.digest)
       async.series([
-          async.apply(fs.unlink, path.join(first2, last38)),
-          async.apply(fs.rmdir, first2),
-          async.apply(fs.rmdir, testStorePath)
-        ], done);
-    });
-  });
-});
+        async.apply(fs.unlink, path.join(first2, last38)),
+        async.apply(fs.rmdir, first2),
+        async.apply(fs.rmdir, testStorePath)
+      ], done)
+    })
+  })
+})
